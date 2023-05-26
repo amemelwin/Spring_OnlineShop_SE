4af0b9e2637adc722cdbcf63c9918f393df1f61e
Ame <mrs.amemelwin@gmail.com>
2023-05-26 18:15:55 +0630

    PUSH

--- src/main/java/com/onlie/shop/controller/OnlineShopController.java
+++ src/main/java/com/onlie/shop/controller/OnlineShopController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,7 +39,8 @@ public class OnlineShopController {
 		return "screens/login";
 	}
 	
-	@PostMapping("/login")
+	
+	@PostMapping(value="/login",params="confirm")
 	public String login(Model model,@Valid @ModelAttribute("loginForm") LoginForm loginForm,BindingResult result,HttpSession session) {
 		if(result.hasErrors()) {
 			return "screens/login";
@@ -45,7 +48,6 @@ public class OnlineShopController {
 		UserEntity authUser = this.shopService.getAuthUser(loginForm.getEmail(),loginForm.getPassword());
 		
 		if (authUser != null) {
-			//session.setAttribute("Auth", user);
 			session.setAttribute("Auth", authUser);
 			return "redirect:/";
 		} else {
@@ -75,23 +77,31 @@ public class OnlineShopController {
 	}
 	
 	@PostMapping("/order/create")
-	public String createOrder(@ModelAttribute OrderForm orderList,Model model) {
+	public String createOrder(@ModelAttribute OrderForm orderList,Model model,HttpSession session) {
+		if (session.getAttribute("Auth") == null) {
+			return "redirect:/login";
+		}
 		model.addAttribute("orderList",orderList.getOrderList());
 		model.addAttribute("divisionList",this.shopService.getAllDivision());
-		// model.addAttribute("has_error","ksjkjskj");  // testing purpose
 		return "screens/order";
 	}
 	
 	@PostMapping("/order/confirm")
-	public String createOrder(@ModelAttribute OrderConfirmForm orderConfirmForm) {
-		// if (result.hasError()){
-		//  model.addAttribute("has_error","error");
-		//  return "screens/order";
-		// }
+	public String createOrder( Model model, @Valid @ModelAttribute OrderConfirmForm orderConfirmForm,BindingResult result,HttpSession session) {
 	// Default user before finish Auth
-	orderConfirmForm.setUserId(1);
+	if(result.hasErrors()) {
+		for(FieldError error: result.getFieldErrors()) {
+			model.addAttribute(error.getField()+"_error", error.getDefaultMessage());
+			
+		}
+		model.addAttribute("has_error","error");
+		model.addAttribute("orderList",orderConfirmForm.getOrderList());
+		model.addAttribute("divisionList",this.shopService.getAllDivision());
+		return "screens/order";
+	}
+	UserEntity user = (UserEntity)session.getAttribute("Auth");
+	orderConfirmForm.setUserId(user.getId());
 	this.shopService.createOrder(orderConfirmForm);
-
 		return "redirect:/";
 	}
 	
--- src/main/java/com/onlie/shop/form/OrderConfirmForm.java
+++ src/main/java/com/onlie/shop/form/OrderConfirmForm.java
@@ -1,14 +1,21 @@
 package com.onlie.shop.form;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
+
 public class OrderConfirmForm {
+	@NotEmpty(message="Receiver Name is required")
 	private String receiverName;
+	
+	@NotEmpty(message="Receiver Phone is required")
 	private String receiverPhone;
+	
+	@NotEmpty(message="Receiver Address is required")
 	private String receiverAddress;
 	private int divisionId;
 	private String orderList;
-	private int userId;   // remove
+	private int userId; // remove later
 
 }
