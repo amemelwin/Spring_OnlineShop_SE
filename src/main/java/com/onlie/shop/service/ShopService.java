d725a71ef0bbb59c4eb9e4e025ea3963edc8235b
Ame <mrs.amemelwin@gmail.com>
2023-05-29 19:43:13 +0630

    space

--- src/main/java/com/onlie/shop/service/ShopService.java
+++ src/main/java/com/onlie/shop/service/ShopService.java
@@ -2,7 +2,6 @@ package com.onlie.shop.service;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
